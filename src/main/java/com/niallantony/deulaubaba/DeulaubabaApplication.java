2342ca668a69e333fe83597c4607887c60fc7f4a
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-06-06 13:55:40 +0900

    Initial Commit

---
+++ src/main/java/com/niallantony/deulaubaba/DeulaubabaApplication.java
@@ -0,0 +1,13 @@
+package com.niallantony.deulaubaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeulaubabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeulaubabaApplication.class, args);
+	}
+
+}
