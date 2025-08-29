59c0a18680a1317140729bcacfbdc17f3dc8d18b
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-08-29 21:48:04 +0900

    Remove unused Imports

--- src/main/java/com/niallantony/deulaubaba/web/StudentController.java
+++ src/main/java/com/niallantony/deulaubaba/web/StudentController.java
@@ -1,12 +1,9 @@
 package com.niallantony.deulaubaba.web;
 
 import com.niallantony.deulaubaba.data.UserRepository;
-import com.niallantony.deulaubaba.domain.User;
 import com.niallantony.deulaubaba.dto.*;
 import com.niallantony.deulaubaba.security.CurrentUser;
 import com.niallantony.deulaubaba.services.StudentServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
