ea885b31e7efee27048d4de3eef509ed797fb74f
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-19 22:17:55 +0900

    Use UserMapper to return entity too

--- src/main/java/com/niallantony/deulaubaba/mapper/UserMapper.java
+++ src/main/java/com/niallantony/deulaubaba/mapper/UserMapper.java
@@ -1,14 +1,38 @@
 package com.niallantony.deulaubaba.mapper;
 
+import com.niallantony.deulaubaba.data.RoleRepository;
+import com.niallantony.deulaubaba.domain.Role;
 import com.niallantony.deulaubaba.domain.User;
 import com.niallantony.deulaubaba.dto.UserDTO;
+import com.niallantony.deulaubaba.dto.UserRequest;
+import com.niallantony.deulaubaba.exceptions.ResourceNotFoundException;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public abstract class UserMapper {
+
+    @Autowired
+    protected RoleRepository roleRepository;
 
     @Mapping(target="user.userId", ignore = true)
     @Mapping(target="user.students", ignore = true)
-    UserDTO toDTO(User user);
+    public abstract UserDTO toDTO(User user);
+
+    @Mapping(target = "students", ignore = true)
+    @Mapping(target = "imagesrc", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    public abstract User toUser(UserRequest userRequest, String userId);
+
+    @AfterMapping
+    protected void setUserRole(@MappingTarget User user) {
+        Role role = roleRepository.findByName("ROLE_USER").orElseThrow(
+                () -> new ResourceNotFoundException("Role does not exist")
+        );
+        user.setRole(role);
+    }
+
 }
--- src/main/java/com/niallantony/deulaubaba/services/UserService.java
+++ src/main/java/com/niallantony/deulaubaba/services/UserService.java
@@ -2,10 +2,8 @@ package com.niallantony.deulaubaba.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.niallantony.deulaubaba.domain.Role;
 import com.niallantony.deulaubaba.domain.Student;
 import com.niallantony.deulaubaba.domain.User;
-import com.niallantony.deulaubaba.data.RoleRepository;
 import com.niallantony.deulaubaba.data.StudentRepository;
 import com.niallantony.deulaubaba.data.UserRepository;
 import com.niallantony.deulaubaba.dto.StudentDTO;
@@ -23,11 +21,9 @@ import java.io.IOException;
 @Service
 public class UserService {
     private final StudentRepository studentRepository;
-    private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final ObjectMapper jacksonObjectMapper;
     private final FileStorageService fileStorageService;
-    private final StudentService studentService;
     private final UserMapper userMapper;
     private final StudentMapper studentMapper;
 
@@ -35,20 +31,16 @@ public class UserService {
     @Autowired
     public UserService(
             StudentRepository studentRepository,
-            RoleRepository roleRepository,
             UserRepository userRepository,
             ObjectMapper jacksonObjectMapper,
             FileStorageService fileStorageService,
-            StudentService studentService,
             UserMapper userMapper,
             StudentMapper studentMapper
     ) {
         this.studentRepository = studentRepository;
-        this.roleRepository = roleRepository;
         this.userRepository = userRepository;
         this.jacksonObjectMapper = jacksonObjectMapper;
         this.fileStorageService = fileStorageService;
-        this.studentService = studentService;
         this.userMapper = userMapper;
         this.studentMapper = studentMapper;
     }
@@ -62,7 +54,7 @@ public class UserService {
 
     public User createUser(String userId, String data) throws ResourceNotFoundException, JsonProcessingException {
         UserRequest userRequest = jacksonObjectMapper.readValue(data, UserRequest.class);
-        User user = newUser(userId, userRequest);
+        User user = userMapper.toUser(userRequest, userId);
         userRepository.save(user);
         return user;
     }
@@ -71,7 +63,7 @@ public class UserService {
         UserRequest userRequest = jacksonObjectMapper.readValue(data, UserRequest.class);
         String filename = fileStorageService.storeImage(image);
 
-        User user = newUser(userId, userRequest);
+        User user = userMapper.toUser(userRequest, userId);
         user.setImagesrc(filename);
         userRepository.save(user);
 
@@ -89,16 +81,4 @@ public class UserService {
         return studentMapper.toDTO(student);
     }
 
-    private User newUser (String userId, UserRequest userRequest) {
-        Role userRole = roleRepository.findByName("ROLE_USER")
-                .orElseThrow(() -> new ResourceNotFoundException("User Role Not Found"));
-        User user = new User();
-        user.setName(userRequest.getName());
-        user.setUsername(userRequest.getUsername());
-        user.setUserType(userRequest.getUserType());
-        user.setEmail(userRequest.getEmail());
-        user.setRole(userRole);
-        user.setUserId(userId);
-        return user;
-    }
 }
