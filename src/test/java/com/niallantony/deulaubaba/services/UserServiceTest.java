2b0ed562ae408d3962717cd77a7798a519e17ca1
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-24 17:22:54 +0900

    Remove unused import

--- src/test/java/com/niallantony/deulaubaba/services/UserServiceTest.java
+++ src/test/java/com/niallantony/deulaubaba/services/UserServiceTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
