8a96463ab5fd668deee4b4966568186962188bd8
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-28 16:25:05 +0900

    Change FileStorageService to accept custom image folders

--- src/main/java/com/niallantony/deulaubaba/services/FileStorageService.java
+++ src/main/java/com/niallantony/deulaubaba/services/FileStorageService.java
@@ -2,6 +2,7 @@ package com.niallantony.deulaubaba.services;
 
 import com.niallantony.deulaubaba.exceptions.FileStorageException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,10 +15,16 @@ import java.util.UUID;
 @Slf4j
 @Service
 public class FileStorageService {
+    private final Path uploadDirectory;
+
+    public FileStorageService(@Value("${upload.dir:uploads}") String uploadDirectory) {
+        this.uploadDirectory = Paths.get(uploadDirectory);
+    }
+
     public String storeImage(MultipartFile image) throws FileStorageException {
         try {
             String filename = UUID.randomUUID() + "-" + image.getOriginalFilename();
-            Path path = Paths.get("uploads", filename);
+            Path path = uploadDirectory.resolve(filename);
             Files.createDirectories(path.getParent());
             image.transferTo(path);
             return filename;
@@ -28,7 +35,7 @@ public class FileStorageService {
 
     public void deleteImage(String oldAvatar) {
         if (oldAvatar != null && !oldAvatar.isEmpty()) {
-            Path oldPath = Paths.get("uploads", oldAvatar);
+            Path oldPath = uploadDirectory.resolve(oldAvatar);
             try {
                 Files.deleteIfExists(oldPath);
             } catch (IOException e) {
