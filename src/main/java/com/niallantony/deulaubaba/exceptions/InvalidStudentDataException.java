50a97af890d863223b4f3a9dc4db9231b97c7796
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-28 20:40:52 +0900

    Create Exception for invalid Student requests

---
+++ src/main/java/com/niallantony/deulaubaba/exceptions/InvalidStudentDataException.java
@@ -0,0 +1,7 @@
+package com.niallantony.deulaubaba.exceptions;
+
+public class InvalidStudentDataException extends RuntimeException {
+    public InvalidStudentDataException(String message) {
+        super(message );
+    }
+}
