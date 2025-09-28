78771795bed7b0685b420845a702df0452f955d9
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-28 13:40:00 +0900

    Add Lombok Constructors

--- src/main/java/com/niallantony/deulaubaba/dto/UserRequest.java
+++ src/main/java/com/niallantony/deulaubaba/dto/UserRequest.java
@@ -1,8 +1,12 @@
 package com.niallantony.deulaubaba.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
     private String username;
     private String name;
