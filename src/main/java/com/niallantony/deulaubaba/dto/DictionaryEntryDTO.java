c2b4242feafb095466ddc651a97c04e1a24579bb
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-28 13:38:40 +0900

    Add Lombok Constructors

--- src/main/java/com/niallantony/deulaubaba/dto/DictionaryEntryDTO.java
+++ src/main/java/com/niallantony/deulaubaba/dto/DictionaryEntryDTO.java
@@ -4,11 +4,13 @@ import com.niallantony.deulaubaba.domain.CommunicationCategoryLabel;
 import com.niallantony.deulaubaba.domain.ExpressionType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class DictionaryEntryDTO {
     private Long id;
     private ExpressionType type;
