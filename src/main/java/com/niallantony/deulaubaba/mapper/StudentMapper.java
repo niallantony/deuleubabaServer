b9e8861219f404d88668a403b767d74c50f6a125
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-28 20:41:36 +0900

    Add toStudent pathway to StudentMapper

--- src/main/java/com/niallantony/deulaubaba/mapper/StudentMapper.java
+++ src/main/java/com/niallantony/deulaubaba/mapper/StudentMapper.java
@@ -2,10 +2,13 @@ package com.niallantony.deulaubaba.mapper;
 
 import com.niallantony.deulaubaba.domain.Student;
 import com.niallantony.deulaubaba.dto.StudentDTO;
+import com.niallantony.deulaubaba.dto.StudentRequest;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
 
     StudentDTO toDTO(Student student);
+
+    Student toStudent(StudentRequest request);
 }
