e43123c9e283d81e00d6e9fc562fbdc59e885683
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-28 20:41:17 +0900

    Create constructors for Challenge and Communication requests

--- src/main/java/com/niallantony/deulaubaba/dto/StudentChallengeRequest.java
+++ src/main/java/com/niallantony/deulaubaba/dto/StudentChallengeRequest.java
@@ -1,8 +1,10 @@
 package com.niallantony.deulaubaba.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class StudentChallengeRequest {
     private String challengesDetails;
 }
