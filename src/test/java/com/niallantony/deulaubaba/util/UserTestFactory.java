712bb02aa57752eda2bf9c7032ed6de2051c2f35
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-30 17:10:57 +0900

    Amend UserTestFactory to give precedence to SQL

--- src/test/java/com/niallantony/deulaubaba/util/UserTestFactory.java
+++ src/test/java/com/niallantony/deulaubaba/util/UserTestFactory.java
@@ -1,21 +1,9 @@
 package com.niallantony.deulaubaba.util;
 
-import com.niallantony.deulaubaba.domain.Role;
-import com.niallantony.deulaubaba.domain.User;
 import com.niallantony.deulaubaba.dto.UserRequest;
 
 public class UserTestFactory {
-    public static User createBasicUser(Role role) {
-        User u = new User();
-        u.setUserId("user");
-        u.setImagesrc("./example.png");
-        u.setUsername("username");
-        u.setName("name");
-        u.setEmail("email@gmail.com");
-        u.setUserType("user type");
-        u.setRole(role);
-        return u;
-    }
+
 
     public static UserRequest createUserRequest() {
         UserRequest u = new UserRequest();
