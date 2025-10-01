49f034842055ea83312bbb6c717ae8f18d303758
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-01 15:11:51 +0900

    BadRequest method

--- src/test/java/com/niallantony/deulaubaba/util/StudentTestFactory.java
+++ src/test/java/com/niallantony/deulaubaba/util/StudentTestFactory.java
@@ -1,17 +1,24 @@
 package com.niallantony.deulaubaba.util;
 
-import com.niallantony.deulaubaba.domain.Student;
+import com.niallantony.deulaubaba.dto.StudentRequest;
 
 public class StudentTestFactory {
-    public static Student createStudent() {
-        Student student = new Student();
-        student.setStudentId("ABC");
-        student.setName("John");
-        student.setSchool("School");
-        student.setAge(12);
-        student.setGrade(3);
-        student.setSetting("Setting");
-        student.setDisability("Disability");
-        return student;
+    public static StudentRequest createStudentRequest(String uid) {
+        StudentRequest studentRequest = new StudentRequest();
+        studentRequest.setUid(uid);
+        studentRequest.setName("John");
+        studentRequest.setSchool("School");
+        studentRequest.setAge(12);
+        studentRequest.setGrade(3);
+        studentRequest.setSetting("Setting");
+        studentRequest.setDisability("Disability");
+        studentRequest.setImgsrc("./example.png");
+        return studentRequest;
+    }
+
+    public static StudentRequest createBadRequest(String uid) {
+        StudentRequest studentRequest = new StudentRequest();
+        studentRequest.setUid(uid);
+        return studentRequest;
     }
 }
