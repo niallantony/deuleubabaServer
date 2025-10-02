de1a72c462f4fc507efd8e49394cf506ed67f513
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-02 22:51:47 +0900

    Write addDictionary tests

--- src/test/java/com/niallantony/deulaubaba/web/DictionaryControllerTest.java
+++ src/test/java/com/niallantony/deulaubaba/web/DictionaryControllerTest.java
@@ -1,17 +1,22 @@
 package com.niallantony.deulaubaba.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.niallantony.deulaubaba.data.CommunicationCategoryRepository;
 import com.niallantony.deulaubaba.data.DictionaryRepository;
 import com.niallantony.deulaubaba.data.StudentRepository;
 import com.niallantony.deulaubaba.data.UserRepository;
 import com.niallantony.deulaubaba.domain.CommunicationCategory;
-import com.niallantony.deulaubaba.domain.CommunicationCategoryLabel;
+import com.niallantony.deulaubaba.domain.DictionaryEntry;
+import com.niallantony.deulaubaba.dto.DictionaryPostRequest;
+import com.niallantony.deulaubaba.exceptions.FileStorageException;
 import com.niallantony.deulaubaba.services.FileStorageService;
+import com.niallantony.deulaubaba.util.DictionaryTestFactory;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -22,12 +27,16 @@ import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Dictionary;
 import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,7 +55,11 @@ public class DictionaryControllerTest {
     @Autowired
     UserRepository userRepository;
 
-    @Autowired CommunicationCategoryRepository communicationCategoryRepository;
+    @Autowired
+    CommunicationCategoryRepository communicationCategoryRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
 
     @MockitoBean
@@ -149,5 +162,127 @@ public class DictionaryControllerTest {
            .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.message").value("Unauthorized access"))
            .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+    }
+
+    @Test
+    @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+    @Sql("/fixtures/student_and_user.sql")
+    @Sql(scripts = "/fixtures/teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDictionary_withValidData_returns201(@Autowired MockMvc mvc) throws Exception {
+        DictionaryPostRequest request = DictionaryTestFactory.createDictionaryPostRequest();
+        String json = objectMapper.writeValueAsString(request);
+        MockMultipartFile data = new MockMultipartFile("data", "test.json", "application/json", json.getBytes());
+        mvc.perform(multipart("/dictionary")
+                .file(data)
+                .with(jwt()))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+        List<DictionaryEntry> dictionaries = dictionaryRepository.findAll().stream().toList();
+        assertEquals(1, dictionaries.size());
+    }
+
+    @Test
+    @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+    @Sql("/fixtures/student_and_user.sql")
+    @Sql(scripts = "/fixtures/teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDictionary_withValidDataAndImage_returns201(@Autowired MockMvc mvc) throws Exception {
+        when(fileStorageService.storeImage(any())).thenReturn("test.jpg");
+        DictionaryPostRequest request = DictionaryTestFactory.createDictionaryPostRequest();
+        String json = objectMapper.writeValueAsString(request);
+        MockMultipartFile data = new MockMultipartFile("data", "test.json", "application/json", json.getBytes());
+        MockMultipartFile image = new MockMultipartFile("image", "test.jpg", "image/jpeg", "image".getBytes());
+        mvc.perform(multipart("/dictionary")
+                   .file(data)
+                   .file(image)
+                   .with(jwt()))
+           .andExpect(status().isCreated())
+           .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.imgsrc").value("test.jpg"))
+           .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+        List<DictionaryEntry> dictionaries = dictionaryRepository.findAll().stream().toList();
+        verify(fileStorageService).storeImage(any());
+        assertEquals(1, dictionaries.size());
+    }
+
+    @Test
+    @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+    @Sql("/fixtures/student_and_user.sql")
+    @Sql(scripts = "/fixtures/teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDictionary_withValidDataAndImageError_stillReturns201(@Autowired MockMvc mvc) throws Exception {
+        when(fileStorageService.storeImage(any())).thenThrow(FileStorageException.class);
+        DictionaryPostRequest request = DictionaryTestFactory.createDictionaryPostRequest();
+        String json = objectMapper.writeValueAsString(request);
+        MockMultipartFile data = new MockMultipartFile("data", "test.json", "application/json", json.getBytes());
+        MockMultipartFile image = new MockMultipartFile("image", "test.jpg", "image/jpeg", "image".getBytes());
+        mvc.perform(multipart("/dictionary")
+                   .file(data)
+                   .file(image)
+                   .with(jwt()))
+           .andExpect(status().isCreated())
+           .andExpect(jsonPath("$.id").value(1))
+           .andExpect(jsonPath("$.imgsrc").isEmpty())
+           .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+        List<DictionaryEntry> dictionaries = dictionaryRepository.findAll().stream().toList();
+        verify(fileStorageService).storeImage(any());
+        assertEquals(1, dictionaries.size());
+    }
+
+    @Test
+    @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+    @Sql("/fixtures/student_and_user.sql")
+    @Sql(scripts = "/fixtures/teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDictionary_withinValidData_returns400(@Autowired MockMvc mvc) throws Exception {
+        DictionaryPostRequest request = DictionaryTestFactory.createBadRequest();
+        String json = objectMapper.writeValueAsString(request);
+        MockMultipartFile data = new MockMultipartFile("data", "test.json", "application/json", json.getBytes());
+        mvc.perform(multipart("/dictionary")
+                   .file(data)
+                   .with(jwt()))
+           .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Entry data not valid"))
+           .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+        List<DictionaryEntry> dictionaries = dictionaryRepository.findAll().stream().toList();
+        assertEquals(0, dictionaries.size());
+    }
+
+    @Test
+    @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+    @Sql({
+            "/fixtures/student.sql",
+            "/fixtures/user.sql"
+    })
+    @Sql(scripts = "/fixtures/teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDictionary_withUnauthorisedUser_returns401(@Autowired MockMvc mvc) throws Exception {
+        DictionaryPostRequest request = DictionaryTestFactory.createDictionaryPostRequest();
+        String json = objectMapper.writeValueAsString(request);
+        MockMultipartFile data = new MockMultipartFile("data", "test.json", "application/json", json.getBytes());
+        mvc.perform(multipart("/dictionary")
+                   .file(data)
+                   .with(jwt()))
+           .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").value("Unauthorized access"))
+           .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+        List<DictionaryEntry> dictionaries = dictionaryRepository.findAll().stream().toList();
+        assertEquals(0, dictionaries.size());
+    }
+
+    @Test
+    @SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+    @Sql("/fixtures/student_and_user.sql")
+    @Sql(scripts = "/fixtures/teardown.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void addDictionary_withInvalidStudent_returns404(@Autowired MockMvc mvc) throws Exception {
+        DictionaryPostRequest request = DictionaryTestFactory.createDictionaryPostRequest();
+        request.setStudentId("def");
+        String json = objectMapper.writeValueAsString(request);
+        MockMultipartFile data = new MockMultipartFile("data", "test.json", "application/json", json.getBytes());
+        mvc.perform(multipart("/dictionary")
+                   .file(data)
+                   .with(jwt()))
+           .andExpect(status().isNotFound())
+           .andExpect(jsonPath("$.message").value("Student not found def"))
+           .andDo((r) -> System.out.println(r.getResponse().getContentAsString()));
+        List<DictionaryEntry> dictionaries = dictionaryRepository.findAll().stream().toList();
+        assertEquals(0, dictionaries.size());
     }
 }
