e927277a053883f79881966b81cd8a7174a5bb52
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-03 20:23:19 +0900

    Create get all by user ID method

--- src/main/java/com/niallantony/deulaubaba/services/ProjectService.java
+++ src/main/java/com/niallantony/deulaubaba/services/ProjectService.java
@@ -1,24 +1,36 @@
 package com.niallantony.deulaubaba.services;
 
 import com.niallantony.deulaubaba.data.ProjectRepository;
+import com.niallantony.deulaubaba.data.UserRepository;
 import com.niallantony.deulaubaba.domain.Project;
+import com.niallantony.deulaubaba.domain.ProjectUser;
+import com.niallantony.deulaubaba.domain.User;
 import com.niallantony.deulaubaba.dto.ProjectDTO;
+import com.niallantony.deulaubaba.dto.ProjectPreviewDTO;
 import com.niallantony.deulaubaba.exceptions.ResourceNotFoundException;
+import com.niallantony.deulaubaba.exceptions.UserNotAuthorizedException;
 import com.niallantony.deulaubaba.mapper.ProjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
+    private final UserRepository userRepository;
 
     public ProjectService(
             ProjectRepository projectRepository,
-            ProjectMapper projectMapper
+            ProjectMapper projectMapper,
+            UserRepository userRepository
     ) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userRepository = userRepository;
     }
 
     public ProjectDTO getProject(String project_id, String user_id) {
@@ -26,6 +38,21 @@ public class ProjectService {
         Project project = projectRepository.findById(longProjectId).orElseThrow(
                 () -> new ResourceNotFoundException("Project not found")
         );
+        if (!userAssignedToProject(user_id, project)) {
+            throw new UserNotAuthorizedException("Unauthorized access");
+        }
         return projectMapper.toDTO(project);
+    }
+
+    public Set<ProjectPreviewDTO> getProjectsOfUser(String user_id) {
+        User user = userRepository.findByUserId(user_id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return projectRepository.findAllProjectsByUserId(user);
+    }
+
+    private boolean userAssignedToProject(String user_id, Project project) {
+        return project.getUsers().stream()
+                .filter(projectUser -> projectUser.getUser() != null)
+                .anyMatch(projectUser -> projectUser.getUser().getUserId().equals(user_id));
     }
 }
