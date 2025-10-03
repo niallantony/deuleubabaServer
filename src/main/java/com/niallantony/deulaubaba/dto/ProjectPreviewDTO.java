1758fdf461864384196e3df98a6040b3fb111109
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-03 20:21:31 +0900

    Small DTO for project previews

---
+++ src/main/java/com/niallantony/deulaubaba/dto/ProjectPreviewDTO.java
@@ -0,0 +1,19 @@
+package com.niallantony.deulaubaba.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectPreviewDTO {
+    private Long id;
+    private boolean completed;
+    private String description;
+    private String objective;
+    private String imgsrc;
+    private LocalDate startedOn;
+}
