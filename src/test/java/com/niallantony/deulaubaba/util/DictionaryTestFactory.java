6f02367bcb3c60ca6d020c1bfeb6e220adeecb00
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-03 11:33:03 +0900

    Create put requests

--- src/test/java/com/niallantony/deulaubaba/util/DictionaryTestFactory.java
+++ src/test/java/com/niallantony/deulaubaba/util/DictionaryTestFactory.java
@@ -3,6 +3,8 @@ package com.niallantony.deulaubaba.util;
 import com.niallantony.deulaubaba.domain.CommunicationCategoryLabel;
 import com.niallantony.deulaubaba.domain.ExpressionType;
 import com.niallantony.deulaubaba.dto.DictionaryPostRequest;
+import com.niallantony.deulaubaba.dto.DictionaryPutRequest;
+
 
 public class DictionaryTestFactory {
     public static DictionaryPostRequest createDictionaryPostRequest() {
@@ -11,7 +13,6 @@ public class DictionaryTestFactory {
         d.setType(ExpressionType.BODY);
         d.setTitle("Title");
         d.getCategory().add(CommunicationCategoryLabel.PAIN);
-        d.setImgsrc("example.jpg");
         d.setDescription("Description");
         return d;
     }
@@ -19,6 +20,24 @@ public class DictionaryTestFactory {
     public static DictionaryPostRequest createBadRequest() {
         DictionaryPostRequest d = new DictionaryPostRequest();
         d.setStudentId("abc");
+        return d;
+    }
+
+    public static DictionaryPutRequest createDictionaryPutRequest() {
+        DictionaryPutRequest d = new DictionaryPutRequest();
+        d.setId(1L);
+        d.setStudentId("abc");
+        d.setType(ExpressionType.BODY);
+        d.setTitle("New Title");
+        d.getCategory().add(CommunicationCategoryLabel.PAIN);
+        d.getCategory().add(CommunicationCategoryLabel.SHOWME);
+        d.setDescription("New Description");
+        return d;
+    }
+
+    public static DictionaryPutRequest createBadPutReqeust() {
+        DictionaryPutRequest d = new DictionaryPutRequest();
+        d.setId(123L);
         return d;
     }
 }
