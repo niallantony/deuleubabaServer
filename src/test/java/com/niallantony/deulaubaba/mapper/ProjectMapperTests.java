1a032b86caf064ba56c0cad09aab803bb1cae39c
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-03 17:55:47 +0900

    Test mapper

---
+++ src/test/java/com/niallantony/deulaubaba/mapper/ProjectMapperTests.java
@@ -0,0 +1,52 @@
+package com.niallantony.deulaubaba.mapper;
+
+import com.niallantony.deulaubaba.domain.*;
+import com.niallantony.deulaubaba.dto.ProjectDTO;
+import com.niallantony.deulaubaba.dto.ProjectUserStatus;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProjectMapperTests {
+
+    @Test
+    void entityToDto_whenGivenProjectEntry_thenReturnProjectDto() {
+        ProjectMapper projectMapper = new ProjectMapperImpl();
+        CommunicationCategory mockCategory = new CommunicationCategory();
+        mockCategory.setLabel(CommunicationCategoryLabel.ATTENTION);
+        Student student = new Student();
+        LocalDate start = LocalDate.of(2020, 1, 1);
+        LocalDate end = LocalDate.of(2020, 12, 31);
+        User user = new User();
+        user.setUserType("teacher");
+        user.setImagesrc("useravatar.jpg");
+        user.setUsername("username");
+        ProjectUser projectUser = new ProjectUser();
+        projectUser.setUser(user);
+
+        Project project = new Project();
+        project.setId(123L);
+        project.getCategories().add(mockCategory);
+        project.setStudent(student);
+        project.setStartedOn(start);
+        project.setDescription("Description");
+        project.setImgsrc("Image");
+        project.setType(ProjectType.COMMUNICATION);
+        project.setCompletedOn(end);
+        project.getUsers().add(projectUser);
+
+
+        ProjectDTO dto = projectMapper.toDTO(project);
+
+        assertEquals(project.getId(), dto.getId());
+        assertEquals(project.getCategories(), dto.getCategories());
+        assertEquals(project.getStartedOn(), dto.getStartedOn());
+        assertEquals(project.getDescription(), dto.getDescription());
+        assertEquals(project.getImgsrc(), dto.getImgsrc());
+        assertEquals(project.getType(), dto.getType());
+        assertEquals(project.getCompletedOn(), dto.getCompletedOn());
+        assertEquals(1, dto.getUserStatuses().size());
+    }
+}
