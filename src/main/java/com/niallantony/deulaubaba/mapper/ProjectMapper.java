72044219869a1c935ef7550bad21158e5ca7879c
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-03 17:55:39 +0900

    Create mapper to map Projects to DTO including returning only ProjectUserStatus dto

---
+++ src/main/java/com/niallantony/deulaubaba/mapper/ProjectMapper.java
@@ -0,0 +1,35 @@
+package com.niallantony.deulaubaba.mapper;
+
+import com.niallantony.deulaubaba.domain.Project;
+import com.niallantony.deulaubaba.domain.ProjectUser;
+import com.niallantony.deulaubaba.domain.User;
+import com.niallantony.deulaubaba.dto.ProjectDTO;
+import com.niallantony.deulaubaba.dto.ProjectUserStatus;
+import com.niallantony.deulaubaba.dto.UserAvatar;
+import com.niallantony.deulaubaba.exceptions.ResourceNotFoundException;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface ProjectMapper {
+    @Mapping(source = "users", target = "userStatuses")
+    ProjectDTO toDTO(Project project);
+
+
+    default Set<ProjectUserStatus> projectUserToProjectUserStatus(Set<ProjectUser> projectUsers) {
+        if (projectUsers.isEmpty()) return null;
+        return projectUsers.stream().map((user) -> new ProjectUserStatus(
+                new UserAvatar(
+                        user.getUser().getUsername(),
+                        user.getUser().getImagesrc(),
+                        user.getUser().getUserType()
+                ),
+                user.getIsCompleted(),
+                user.getCompletedOn()
+        )).collect(Collectors.toSet());
+    }
+}
+
