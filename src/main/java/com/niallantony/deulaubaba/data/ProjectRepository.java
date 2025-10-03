62b1e3b0f238f70ac6b999844b02e22ac729f93a
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-10-03 20:23:05 +0900

    Create repository query for getting all by UserId

--- src/main/java/com/niallantony/deulaubaba/data/ProjectRepository.java
+++ src/main/java/com/niallantony/deulaubaba/data/ProjectRepository.java
@@ -1,7 +1,20 @@
 package com.niallantony.deulaubaba.data;
 
 import com.niallantony.deulaubaba.domain.Project;
+import com.niallantony.deulaubaba.domain.User;
+import com.niallantony.deulaubaba.dto.ProjectPreviewDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Query(
+            "SELECT new com.niallantony.deulaubaba.dto.ProjectPreviewDTO(" +
+                    "p.id, p.completed, p.description, p.objective, p.imgsrc, p.startedOn) " +
+                    "FROM Project p JOIN p.users u WHERE u.user = :user"
+    )
+    Set<ProjectPreviewDTO> findAllProjectsByUserId(@Param("user") User user) ;
 }
